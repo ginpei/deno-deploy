6511cef785faa27e14cee73794855ee0c38ff25a
ginpei <ginpei@ginpei.dev>
2021-03-31 10:46:10 -0700

    feat: 🎸 HTML

--- ginpei/index.ts
+++ ginpei/index.ts
@@ -1,6 +1,25 @@
+const html = String.raw;
+
 addEventListener("fetch", (event) => {
-  const response = new Response("Hello Ginpei World! 🐧", {
-    headers: { "content-type": "text/plain" },
+  const content = getContent();
+
+  const response = new Response(content, {
+    headers: {
+      "content-type": "text/html; charset=utf-8",
+    },
   });
   event.respondWith(response);
 });
+
+function getContent() {
+  return html`
+<html>
+  <head>
+    <title>Hello Ginpei World! 🐧</title>
+  </head>
+  <body>
+    <h1>Hello Ginpei World! 🐧</h1>
+  </body>
+</html>
+  `.trim();
+}
